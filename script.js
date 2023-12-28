f08430923c56518ad16cb7a787e9719342bbd8b3
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2023-12-28 22:08:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-function firstNonRepeatedChar(str) {
- // Write your code here
+function firstNonRepeatedChar(str) { // Use an object to store character frequencies
+    const frequencyMap = {};
+
+    // Iterate through the string to count character frequencies
+    for (const char of inputString) {
+        // Update frequencyMap for the current character
+        frequencyMap[char] = (frequencyMap[char] || 0) + 1;
+    }
+
+    // Iterate through the string to find the first non-repeated character
+    for (const char of inputString) {
+        if (frequencyMap[char] === 1) {
+            return char;
+        }
+    }
+
+    // If no non-repeated character is found, return null
+    return null;
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
